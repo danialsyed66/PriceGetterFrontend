f2645725db25511e75b9886e3847215b205390bd
noman-noob <67287206+naumanashraf12@users.noreply.github.com>
2022-05-19 12:34:23 -0700

    s
    ssad

--- src/components/users/Wishlist.js
+++ src/components/users/Wishlist.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 import wish from "../../assets/wishlist.svg";
 import heart from "../../assets/hearted.svg";
 import "./wishlist.css";
 import { Footer, Navbar } from "../layouts";
+import { Link } from "react-router-dom";
 const Wishlist = () => {
   return (
     <div
@@ -21,9 +22,61 @@ const Wishlist = () => {
             </p>
           </div>
           <div>
-            <img style={{ width: "400px" }} src={wish} alt="" />
+            <img className="imgwishlist" src={wish} alt="" />
           </div>
         </div>
+        {/* <div className="col-12 col-lg-8">
+          {Items.map(
+            ({
+              _id,
+              name,
+              price,
+              description,
+              rating,
+              images,
+              seller,
+              stock,
+              noOfReviews,
+              category,
+              reviews,
+              createdAt,
+              quantity,
+            }) => (
+              <Fragment key={_id}>
+                <hr />
+                <div className="cart-item">
+                  <div className="row">
+                    <div className="col-4 col-lg-2">
+                      <img
+                        src={images[0]?.url}
+                        alt={"Laptop"}
+                        height="90"
+                        width="115"
+                      />
+                    </div>
+
+                    <div className="col-5 col-lg-4">
+                      <Link to={`/product/${_id}`}>{name}</Link>
+                    </div>
+
+                    <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                      <p id="card_item_price">Rs. {price}</p>
+                    </div>
+
+                    <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                      <i
+                        id="delete_cart_item"
+                        className="fa fa-trash btn btn-danger"
+                        onClick={() => {}}
+                      ></i>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+              </Fragment>
+            )
+          )}
+        </div> */}
       </div>
       <div style={{ marginTop: "auto" }}>
         <Footer />
